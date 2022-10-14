5cc3aee748ef2bfbe2e2eea1c0e3075e78d8dd71
bebeloper <kevind171819@gmail.com>
2022-10-13 20:50:58 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-store';
+  parentImg: string = 'https://www.whatsappimages.in/wp-content/uploads/2021/12/girl-New-Superb-Whatsapp-Dp-Profile-Images-photo.jpg';
 }
---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.css']
+})
+export class ImgComponent implements OnInit {
+
+  @Input() img: string = '';
+  defaultImg: string = 'https://180dc.org/wp-content/uploads/2016/08/default-profile.png';
+  defaultTextImg: string = 'Inserte una imagen';
+  errorImg: boolean = false;
+  imageLoaded: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  imageError(){
+    this.errorImg = true;
+    if (this.errorImg) {
+      console.log('Hay un error: ' + this.errorImg);
+      this.img = this.defaultImg;
+    }
+  }
+
+  imgLoaded(){
+    this.imageLoaded = true;
+    if (this.imageLoaded && this.errorImg) {
+      this.defaultTextImg = 'Error al cargar la imagen';
+      console.log('Imagen cargada: ' + this.imageLoaded);
+      this.errorImg = false;
+    }else if (this.imageLoaded && this.errorImg == false) {
+      this.defaultTextImg = 'Imagen cargada exitosamente';
+      console.log('Imagen cargada: ' + this.imageLoaded);
+      console.log('Hay un error: ' + this.errorImg);
+    }
+  }
+
+}
